61fbb2ba7576631a62974ce2933027156cbfcfa7
Dany Luna <danielacrisantoluna@gmail.com>
2025-06-30 13:51:23 -0600

    30 de Julio

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -1,38 +1,48 @@
 import { Request, Response } from 'express';
 import colors from 'colors';
 import Product from '../models/Product.mo';
-import { handleCreateProductErrors } from '../middleware/handleCreateProductErrors';
-import { handleUpdateProductErrors } from '../middleware/handleUpdateProductErrors';
-import { handleDeleteProductErrors } from '../middleware/handleDeleteProductErrors';
-import { handleGetByIDProductErrors } from '../middleware/handleGetByIDProductErrors';
 
-export const createProduct = [
-    handleCreateProductErrors,
-    async (req: Request, res: Response) => {
+export const createProduct = async (req: Request, res: Response) => {
+    try {
         const product = await Product.create(req.body);
         res.json({ data: product });
+    } catch (error) {
+        console.error(colors.red.bold("Error al crear el producto: " + error));
     }
-];
-
-export const getAllProducts = async (req: Request, res: Response) => {
-    const products = await Product.findAll();
-    console.log(colors.green.bold("Obteniendo todos los productos"));
-    res.json({ data: products });
 }
 
-export const getProductById = [handleGetByIDProductErrors,
-    async (req: Request, res: Response) => {
+
+export const getAllProducts = async (req: Request, res: Response) => {
+    try {
+        const products = await Product.findAll(
+            {
+                order: [['price', 'DESC']],
+
+            }
+        );
+        console.log(colors.green.bold("Obteniendo todos los productos"));
+        res.json({ data: products });
+    } catch (error) {
+        console.error(colors.red.bold("Error al obtener los productos: " + error));
+    }
+}
+
+export const getProductById = async (req: Request, res: Response) => {
+    try {
         const { id } = req.params;
         const product = await Product.findByPk(id);
         if (!product) {
             return res.status(404).json({ error: 'Producto no encontrado' });
         }
         res.json({ data: product });
+    } catch (error) {
+        console.error(colors.red.bold("Error al obtener el producto: " + error));
     }
-];
+}
 
-export const updateProduct = [handleUpdateProductErrors,
-    async (req: Request, res: Response) => {
+
+export const updateProduct = async (req: Request, res: Response) => {
+    try {
         const { id } = req.params;
         const product = await Product.findByPk(id);
         if (!product) {
@@ -40,17 +50,7 @@ export const updateProduct = [handleUpdateProductErrors,
         }
         await product.update(req.body);
         res.json({ data: product });
+    } catch (error) {
+        console.error(colors.red.bold("Error al actualizar el producto: " + error));
     }
-];
-
-export const deleteProduct = [handleDeleteProductErrors,
-    async (req: Request, res: Response) => {
-        const { id } = req.params;
-        const product = await Product.findByPk(id);
-        if (!product) {
-            return res.status(404).json({ error: 'Producto no encontrado' });
-        }
-        await product.destroy();
-        res.json({ message: 'Producto eliminado exitosamente' });
-    }
-];
+}
--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,45 @@
 import { Router } from 'express';
-import { createProduct, getAllProducts, getProductById, updateProduct, deleteProduct } from './handlers/product';
+import { createProduct, getAllProducts, getProductById, updateProduct } from './handlers/product';
 import { handleInputErrors } from './middleware';
+import { body, param } from 'express-validator';
+
 
 const router = Router();
 router.get('/', handleInputErrors, getAllProducts);
 
-router.get('/:id', handleInputErrors, getProductById);
+router.get('/:id',
+    param('id')
+        .notEmpty().withMessage('El ID del producto es obligatorio')
+        .isNumeric().withMessage('El ID debe ser un número')
+        .custom(value => value > 0).withMessage('El ID debe ser mayor que 0'),
+    handleInputErrors, getProductById);
 
-router.post('/', handleInputErrors, createProduct);
+router.post('/',
+    body('name')
+        .notEmpty().withMessage('El nombre es obligatorio')
+        .isLength({ min: 3 }).withMessage('El nombre debe tener al menos 3 caracteres')
+        .isLength({ max: 100 }).withMessage('El nombre no puede tener más de 100 caracteres'),
 
-router.put('/:id', handleInputErrors,updateProduct );
+    body('price')
+        .notEmpty().withMessage('El precio es obligatorio')
+        .isNumeric().withMessage('El precio debe ser un número')
+        .custom(value => value > 0).withMessage('El precio debe ser mayor que 0'),
+    handleInputErrors, createProduct);
 
-router.delete('/:id', handleInputErrors, deleteProduct);
+router.put('/:id',
+    param('id')
+        .notEmpty().withMessage('El ID del producto es obligatorio')
+        .isNumeric().withMessage('El ID debe ser un número')
+        .custom(value => value > 0).withMessage('El ID debe ser mayor que 0'),
+    body('name')
+        .notEmpty().withMessage('El nombre es obligatorio')
+        .isLength({ min: 3 }).withMessage('El nombre debe tener al menos 3 caracteres')
+        .isLength({ max: 100 }).withMessage('El nombre no puede tener más de 100 caracteres'),
+
+    body('price')
+        .notEmpty().withMessage('El precio es obligatorio')
+        .isNumeric().withMessage('El precio debe ser un número')
+        .custom(value => value > 0).withMessage('El precio debe ser mayor que 0'),
+    handleInputErrors, updateProduct);
 
 export default router;
