8a8eae3d8327f1aa83ab141825d8bf58c18839a4
Dany Luna <danielacrisantoluna@gmail.com>
2025-06-30 13:50:27 -0600

    Clase: Julio 30

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,7 +3,6 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-//const db  = new Sequelize('postgresql://pern_test_api_k7cx_user:xsxwfgouIwj9WEWxnB4qN5sTmqs6Dlp5@dpg-d168bmvdiees739ik8fg-a.oregon-postgres.render.com/pern_test_api_k7cx');
 const db = new Sequelize(process.env.BD_URL,{
     models: [__dirname + '/../models/**/*.ts']
 });
